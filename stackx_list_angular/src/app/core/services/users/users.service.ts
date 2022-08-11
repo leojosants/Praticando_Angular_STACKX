1a9a9f47c7539a85eb66c2361eba855d033d6690
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-11 14:56:06 -0300

    Parte I

---
+++ stackx_list_angular/src/app/core/services/users/users.service.ts
@@ -0,0 +1,22 @@
+//
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getUsers(quantityUsers: string) : Observable<any> {
+    return this.http.get(`${environment.userApiUrl}?results=30${quantityUsers}`);
+  }
+
+}
--- stackx_list_angular/src/environments/environment.ts
+++ stackx_list_angular/src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  userApiUrl: 'https://randomuser.me/api/',
+
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
